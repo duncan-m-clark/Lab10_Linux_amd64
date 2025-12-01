2a8353b3a8672dee5ec129b1782f9fd3a9daa629
BenG5-9 <benguest23@gmail.com>
2025-12-01 02:37:08 -0500

    starting dynamic prrotecttions

--- LabCrackme.c
+++ LabCrackme.c
@@ -36,85 +36,115 @@ BOOL doCheck(char user[], unsigned char* key);
 
 BOOL doCheckConvert(char user[], char keychars[]) {
 
-	//DEBUGLINE;
 	SALT
-	if (strlen(keychars) != 32) {
-		return FALSE;
+	pid_t pid = fork();
+
+	if(pid == 0){
+		struct timespec start;
+		clock_gettime(CLOCK_REALTIME, &start);
 	}
-
-	//DEBUGLINE;
-
-	unsigned char key[16];
-
-	char temp[3] = { 0 };
-	char* check;
-	SALT
-	for (int i = 0; i < 16; i++) {
-		memcpy(temp, &keychars[2 * i], 2);
-		key[i] = strtol(temp, &check, 16);
-#ifdef _DEBUG
-		fprintf(stderr, "key[%d] = %02hhx\n", i, key[i]);
-#endif
-		if (check != &temp[2]) {
+	else{
+		
+		//DEBUGLINE;
+		SALT
+		if (strlen(keychars) != 32) {
 			return FALSE;
+		}
+
+		//DEBUGLINE;
+
+		unsigned char key[16];
+
+		char temp[3] = { 0 };
+		char* check;
+		SALT
+		for (int i = 0; i < 16; i++) {
+			memcpy(temp, &keychars[2 * i], 2);
+			key[i] = strtol(temp, &check, 16);
+	#ifdef _DEBUG
+			fprintf(stderr, "key[%d] = %02hhx\n", i, key[i]);
+	#endif
+			if (check != &temp[2]) {
+				return FALSE;
+			}
+		}
+
+		//DEBUGLINE;
+	}
+	SALT
+	if(pid == 0){
+		struct timespec stop;
+		clock_gettime(CLOCK_REALTIME, &stop);
+		long time_passed = stop.tv_nsec - start.tv_nsec;
+		if(time_passed > 100000000000) {
+			printf("Debugger detected. Closing\n");
+			pid_t parent_id = getppid();
+			kill(parent_id, SIGKILL); 
+			return 0;
 		}
 	}
 
-	//DEBUGLINE;
-	SALT
 	return doCheck(user, key);
 }
 
 BOOL doCheck(char user[], unsigned char* key) {
-	SALT
-	EVP_MD_CTX* mdctx;
 
-	BOOL bResult = FALSE;
 	SALT
-	mdctx = EVP_MD_CTX_create();
-	if (mdctx == NULL) {
-		return FALSE;
+	int read_write_pipe[2];
+	pipe(read_write_pipe);
+	pid_t pid = fork();
+
+	if(pid == 0){
+		close(read_write_pipe[0]);
+		struct timespec start;
+		clock_gettime(CLOCK_REALTIME, &start);
 	}
+	else{
+		SALT
+		close(read_write_pipe[1]);
+		EVP_MD_CTX* mdctx;
 
-	//DEBUGLINE;
-	SALT
+		BOOL bResult = FALSE;
+		SALT
+		mdctx = EVP_MD_CTX_create();
+		if (mdctx == NULL) {
+			return FALSE;
+		}
 
-	//#pid_t pid = fork();
-	//if(pid ==0){
-		//struct timespec start;
-		//clock_gettime(CLOCK_REALTIME, &start)
-	//}
-	bResult = EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
-	if (!bResult) {
+		//DEBUGLINE;
+		SALT
+		bResult = EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+		if (!bResult) {
+			EVP_MD_CTX_destroy(mdctx);
+			return FALSE;
+		}
+
+		//DEBUGLINE;
+		SALT
+		bResult = EVP_DigestUpdate(mdctx, user, strlen(user));
+		if (!bResult) {
+			EVP_MD_CTX_destroy(mdctx);
+			return FALSE;
+		}
+
+		//DEBUGLINE;
+		SALT
+		BYTE MD5Data[20] = { 0 };
+		SALT
+		DWORD cbHash = sizeof(MD5Data);
+		SALT
+		bResult = EVP_DigestFinal_ex(mdctx, MD5Data, NULL);
+		if (!bResult) {
+			EVP_MD_CTX_destroy(mdctx);
+			return FALSE;
+		}
+
+		//DEBUGLINE;
+		SALT
 		EVP_MD_CTX_destroy(mdctx);
-		return FALSE;
+
+		//DEBUGLINE;
 	}
-
-	//DEBUGLINE;
-	SALT
-	bResult = EVP_DigestUpdate(mdctx, user, strlen(user));
-	if (!bResult) {
-		EVP_MD_CTX_destroy(mdctx);
-		return FALSE;
-	}
-
-	//DEBUGLINE;
-	SALT
-	BYTE MD5Data[20] = { 0 };
-	SALT
-	DWORD cbHash = sizeof(MD5Data);
-	SALT
-	bResult = EVP_DigestFinal_ex(mdctx, MD5Data, NULL);
-	if (!bResult) {
-		EVP_MD_CTX_destroy(mdctx);
-		return FALSE;
-	}
-
-	//DEBUGLINE;
-	SALT
-	EVP_MD_CTX_destroy(mdctx);
-
-	//DEBUGLINE;
 
 #if 0
 	printf("SHA1(user) = ");
@@ -123,14 +153,25 @@ BOOL doCheck(char user[], unsigned char* key) {
 	}
 	printf("\n");
 #endif
-
+	SALT
 	WORD checkMD5 = 0;
 	BYTE chain_value = 1;
 	uint8_t shift = 0;
+	WORD pipe_buffer = 0;
 	SALT
+
 	for (int i = 0; i < cbHash; i++) {
 		checkMD5 += MD5Data[i] ^ chain_value;
-		checkMD5 = checkMD5 * 109;
+
+		if(pid == 0){
+			checkMD5 = checkMD5 * 109;
+			write(read_write_pipe[1], &checkMD5, 1)
+		}
+		else{
+			read(read_write_pipe[0], &pipe_buffer, 1);
+			checkMD5 = pipe_buffer;
+		}
+
 		chain_value = MD5Data[i]; 
 	}
 	SALT
@@ -138,10 +179,18 @@ BOOL doCheck(char user[], unsigned char* key) {
 	chain_value = 1;
 	for (int i = 0; i < 16; i++) {
 		checkKey += key[i] ^ chain_value;
-		checkKey = checkKey * 137;
+		if(pid == 0){
+			checkKey = checkKey * 137;
+			write(read_write_pipe[1], &checkKey, 1)
+		}
+		else{
+			read(read_write_pipe[0], &pipe_buffer, 1);
+			checkKey = pipe_buffer;
+		}
 		chain_value = key[i];
 
 	}
+	SALT
 
 #ifdef _DEBUG
 	printf("checkMD5 = %04x, checkKey = %04x\n", checkMD5, checkKey);
@@ -192,6 +241,7 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Error: Please provide a username and key\n");
 		exit(-1);
 	}
+
 
 	if (doCheckConvert(argv[1], argv[2])) {
 		printf("You're winner!\n");
